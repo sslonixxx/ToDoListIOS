9b7c40f455f0987ae9ef2f8bd4da72c3f545c52c
Cофья Гергет <146007213+sslonixxx@users.noreply.github.com>
2024-09-15 20:57:02 +0700

    Initial Commit

---
+++ ToDoListM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListM
+//
+//  Created by Софья Гергет on 15.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListM/ToDoListMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListMApp.swift
+//  ToDoListM
+//
+//  Created by Софья Гергет on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
